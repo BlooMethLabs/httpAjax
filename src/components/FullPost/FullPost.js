8a45252bd3f57176e30d2d648516fe8016ffa410
BlooMethLabs <bmca620@gmail.com>
2020-09-07 16:04:27 +0100

    Get post and delete axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    selectedPost: {},
+  };
 
-        );
-        return post;
+  componentDidUpdate() {
+    const url = 'https://jsonplaceholder.typicode.com/posts/' + this.props.id;
+    if (this.props.id && this.state.selectedPost.id !== this.props.id) {
+      axios.get(url).then((response) => {
+        console.log(response);
+        this.setState({ selectedPost: response.data });
+      });
     }
+  }
+
+  deletePostHandler = () => {
+    axios
+      .delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  render() {
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post</p>;
+    if (!this.state.selectedPost && this.props.id) {
+      post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+    }
+    if (this.state.selectedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.selectedPost.title}</h1>
+          <p>{this.state.selectedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,25 +4,64 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    selectedPostId: null,
+    posts: [],
+    error: false,
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: 'Barry',
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error: true });
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let posts = <p style={{ textAlign: 'center' }}>An error occurred.</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => (
+        <Post
+          title={post.title}
+          key={post.id}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        ></Post>
+      ));
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
